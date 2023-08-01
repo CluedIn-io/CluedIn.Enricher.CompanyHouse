4a55bea32b95f56bbf29145a2fa631498129917e
Oleksandr Povar <opo@cluedin.com>
2023-08-01 12:40:09 +0200

    feat: Update to .NET 6

--- src/ExternalSearch.Providers.CompanyHouse/CompanyHouseExternalSearchProvider.cs
+++ src/ExternalSearch.Providers.CompanyHouse/CompanyHouseExternalSearchProvider.cs
@@ -79,7 +79,7 @@ namespace CluedIn.ExternalSearch.Providers.CompanyHouse
 
                 if (organizationName != null)
                 {
-                    var values = organizationName.Select(NameNormalization.Normalize).ToHashSetEx();
+                    var values = organizationName.Select(NameNormalization.Normalize).ToHashSet();
 
                     foreach (var value in values.Where(v => !nameFilter(v)))
                         yield return new ExternalSearchQuery(this, entityType, ExternalSearchQueryParameter.Name, value);
