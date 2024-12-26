15f13cdb1537a77887988cad244fdccfd2895b96
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2024-12-26 08:58:47 +0800

    fix: Disallow spaces in configuration field (#30)

--- src/ExternalSearch.Providers.CompanyHouse/Constants.cs
+++ src/ExternalSearch.Providers.CompanyHouse/Constants.cs
@@ -54,14 +54,22 @@ namespace CluedIn.ExternalSearch.Providers.CompanyHouse
         {
             Token = new List<Control>
             {
-                new Control() {
+                new()
+                {
                     DisplayName = "API Key",
                     Type = "password",
                     IsRequired = true,
                     Name = KeyName.ApiKey,
-                    Help = "The key to authenticate access to the Companies House API"
+                    Help = "The key to authenticate access to the Companies House API",
+                    ValidationRules = new List<Dictionary<string, string>>()
+                    {
+                        new() {
+                            { "regex", "\\s" },
+                            { "message", "Spaces are not allowed" }
+                        }
+                    },
                 },
-                new Control()
+                new()
                 {
                     DisplayName = "Accepted Entity Type",
                     Type = "entityTypeSelector",
@@ -69,7 +77,7 @@ namespace CluedIn.ExternalSearch.Providers.CompanyHouse
                     Name = KeyName.AcceptedEntityType,
                     Help = "The entity type that defines the golden records you want to enrich (e.g., /Organization)."
                 },
-                new Control()
+                new()
                 {
                     DisplayName = "Companies House Number Vocabulary Key",
                     Type = "vocabularyKeySelector",
@@ -77,7 +85,7 @@ namespace CluedIn.ExternalSearch.Providers.CompanyHouse
                     Name = KeyName.CompanyHouseNumberKey,
                     Help = "The vocabulary key that contains the Company House Number of companies you want to enrich (e.g., organization.companyshousenumber)"
                 },
-                new Control()
+                new()
                 {
                     DisplayName = "Country Vocabulary Key",
                     Type = "vocabularyKeySelector",
@@ -85,7 +93,7 @@ namespace CluedIn.ExternalSearch.Providers.CompanyHouse
                     Name = KeyName.CountryKey,
                     Help = "The vocabulary key that contains the countries of companies you want to enrich (e.g., organization.country)."
                 },
-                new Control()
+                new()
                 {
                     DisplayName = "Organization Name Vocabulary Key",
                     Type = "vocabularyKeySelector",
