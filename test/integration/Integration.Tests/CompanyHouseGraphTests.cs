beb45c873aee42b3d85d0533d603893a2844c4f3
Oleks <opo@cluedin.com>
2026-05-01 14:59:21 +0200

    chore: Resolve build errors after .NET 10 migration
    Replace removed RestSharp 114 APIs: IRestResponse -> RestResponse, Method.GET -> Method.Get.

--- test/integration/Integration.Tests/CompanyHouseGraphTests.cs
+++ test/integration/Integration.Tests/CompanyHouseGraphTests.cs
@@ -8,11 +8,11 @@ using CluedIn.Core.Serialization;
 using CluedIn.Core.Workflows;
 using CluedIn.ExternalSearch;
 using CluedIn.ExternalSearch.Providers.CompanyHouse;
-using CluedIn.Testing.Base.Context;
 using CluedIn.Testing.Base.ExternalSearch;
 using CluedIn.Testing.Base.Processing.Actors;
 using Moq;
 using Xunit;
+using TestContext = CluedIn.Testing.Base.Context.TestContext;
 
 namespace ExternalSearch.CompanyHouse.Integration.Tests
 {
@@ -60,10 +60,10 @@ namespace ExternalSearch.CompanyHouse.Integration.Tests
             context.Workflow = command.Workflow;
 
             // Act
-            var result = actor.ProcessWorkflowStep(context, command);
+            var result = actor.ProcessWorkflowStepAsync(context, command).GetAwaiter().GetResult();
             Assert.Equal(WorkflowStepResult.Repeat.SaveResult, result.SaveResult);
 
-            result = actor.ProcessWorkflowStep(context, command);
+            result = actor.ProcessWorkflowStepAsync(context, command).GetAwaiter().GetResult();
             Assert.Equal(WorkflowStepResult.Success.SaveResult, result.SaveResult);
             context.Workflow.AddStepResult(result);
 
