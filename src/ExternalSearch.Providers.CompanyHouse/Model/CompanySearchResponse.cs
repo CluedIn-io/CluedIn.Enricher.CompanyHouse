08eb1656ccb887e9ee7f325bafa0d628616ea2b2
Yi-Wei <ywei390@gmail.com>
2026-08-05 14:39:03 +0800

    fix: Update CompanySearchResponse model to resolve object not found error

--- src/ExternalSearch.Providers.CompanyHouse/Model/CompanySearchResponse.cs
+++ src/ExternalSearch.Providers.CompanyHouse/Model/CompanySearchResponse.cs
@@ -6,9 +6,9 @@ namespace CluedIn.ExternalSearch.Providers.CompanyHouse.Model
     {
         public string etag { get; set; }
         public List<CompanySearchResultItem> items { get; set; }
-        public string items_per_page { get; set; }
+        public int items_per_page { get; set; }
         public string kind { get; set; }
-        public string start_index { get; set; }
-        public string total_results { get; set; }
+        public int start_index { get; set; }
+        public int total_results { get; set; }
     }
 }
